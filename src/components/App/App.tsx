a1dcc7ddcd85354c8a6629421ef5b15740582c20
udartapkom <karakoom@ya.ru>
2025-02-24 16:42:16 +0300

    fix/problem

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import {
-  Route, Routes, useLocation,
+  Route, Routes,
 } from 'react-router';
 import styles from './App.module.scss';
 import { Auth, Error } from '../../pages/index';
 
-const App = () => {
-  const location = useLocation();
-  // const background = location.state && location.state.background;
-  return (
-    <div className={styles.App}>
-      <Routes>
-        <Route path='/' element={<Auth />} />
-        <Route path='/*' element={<Error />} />
-      </Routes>
-    </div>
-  );
-};
+const App = () => (
+  <div className={styles.App}>
+    <Routes>
+      <Route path='/' element={<Auth />} />
+      <Route path='/*' element={<Error />} />
+    </Routes>
+  </div>
+);
 export default App;
